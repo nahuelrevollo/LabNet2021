a05c34c4a6656c385e55c01b09b588012734f63e
Revollo Nahuel <63524349+nahuelrevollo@users.noreply.github.com>
2021-04-20 12:27:22 -0300

    Add files via upload

---
+++ Trabajo Practico 2 LabNet2021/Dominio/Transporte.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public abstract class  Transporte
+
+
+    {
+        
+
+
+        public int Pasajeros { get; set; }
+
+        public abstract void Avanzar();
+
+
+        public abstract void Detenerse();
+
+
+    }
+}
