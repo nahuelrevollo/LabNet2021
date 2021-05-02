48c02072bb11c56ecf7e377f504a7a713924205d
Revollo Nahuel <63524349+nahuelrevollo@users.noreply.github.com>
2021-05-02 12:53:52 -0300

    Add files via upload

---
+++ LABNET2021_TP_EF_LOGIC/BaseLogic.cs
@@ -0,0 +1,22 @@
+﻿using LABNET2021_TP_EF_DATA;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LABNET2021_TP_EF_LOGIC
+{
+   public class BaseLogic
+    {
+
+        protected readonly NorthwindContext context;
+
+        public BaseLogic()
+        {
+            context = new NorthwindContext();
+        }
+
+
+    }
+}
