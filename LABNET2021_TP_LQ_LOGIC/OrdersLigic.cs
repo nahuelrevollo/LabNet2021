f3b04fff91e704856ebc38526be043bd95c50d54
Revollo Nahuel <63524349+nahuelrevollo@users.noreply.github.com>
2021-05-04 00:42:38 -0300

    Add files via upload

---
+++ LABNET2021_TP_LQ_LOGIC/OrdersLigic.cs
@@ -0,0 +1,18 @@
+﻿using LABNET2021_TP_LQ_ENTITIES;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LABNET2021_TP_LQ_LOGIC
+{
+    public class OrdersLigic : BaseLogic, Interface1<Orders>
+    {
+        public List<Orders> Getall()
+        {
+            return context.Orders.ToList();
+        }
+
+    }
+}
---
+++ LABNET2021_TP_LQ_LOGIC/ProductsLogic.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+using LABNET2021_TP_LQ_ENTITIES;
+
+namespace LABNET2021_TP_LQ_LOGIC
+{
+    public class ProductsLogic : BaseLogic, Interface1<Products>
+    {
+        public List<Products> Getall()
+        {
+            return context.Products.ToList();
+        }
+
+        public List<Products> query2 ()
+        {
+            var query = from products in context.Products
+                        where products.UnitsInStock == 0
+                        select products;
+
+            return query.ToList();
+                    
+
+        }
+
+        public List<Products> query3()
+        {
+            var query = from products in context.Products
+                        where products.UnitsInStock != 0 && products.UnitPrice>3
+                        select products;
+
+            return query.ToList();
+
+
+        }
+
+
+    }
+}
