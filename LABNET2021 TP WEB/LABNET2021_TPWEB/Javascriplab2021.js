986cbee7ae25c23323a2afc72c7803a7c7d0191c
Revollo Nahuel <63524349+nahuelrevollo@users.noreply.github.com>
2021-05-05 00:34:18 -0300

    Add files via upload

---
+++ LABNET2021 TP WEB/LABNET2021_TPWEB/Javascriplab2021.js
@@ -0,0 +1,52 @@
+﻿
+function validarNombre()
+
+{
+    var nombre = document.getElementById("id_nombre").value;
+
+    if ((nombre == "" || !nombre))
+
+    {
+
+        
+
+        alert("completar nombre, campo obligatorio");
+        return false;
+
+    }
+    return true;
+
+}
+
+function validarApellido()
+
+{
+    var apellido = document.getElementById("id_apellido").value;
+
+    if ((apellido == "" || !apellido))
+    {
+       
+
+        alert("completar apellido campo obligatorio");
+        return false;
+
+    }
+
+    return true;
+
+}
+
+
+function limpiarCampos()
+
+{
+    document.getElementById("id_nombre").value= "";
+    document.getElementById("id_apellido").value="";
+    document.getElementById("id_edad").value = "";
+    document.getElementById("id_empresa").value = "";
+
+
+    return true;
+
+
+}
